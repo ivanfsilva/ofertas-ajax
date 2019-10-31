55c998e3b54b8fe440942965aa803e2e1af0e40e
ivanfsilva <ivanfs27@gmail.com>
2019-10-31 13:33:31 -0300

    Projeto Criado

---
+++ src/main/java/br/com/ivanfsilva/ofertasajax/OfertasAjaxApplication.java
@@ -0,0 +1,13 @@
+package br.com.ivanfsilva.ofertasajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfertasAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfertasAjaxApplication.class, args);
+	}
+
+}
