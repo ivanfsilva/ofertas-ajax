00ce5ede732bf22c4cc6ece6cb5107b23f1d8f1a
ivanfsilva <ivanfs27@gmail.com>
2019-10-31 22:08:48 -0300

    função ajax para obter informações da url, atualizar formulário e capturar erros de requisições

---
+++ src/main/java/br/com/ivanfsilva/ofertasajax/web/controller/SocialMetaTagController.java
@@ -0,0 +1,28 @@
+package br.com.ivanfsilva.ofertasajax.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import br.com.ivanfsilva.ofertasajax.domain.SocialMetaTag;
+import br.com.ivanfsilva.ofertasajax.service.SocialMetaTagService;
+
+@Controller
+@RequestMapping("/meta")
+public class SocialMetaTagController {
+
+	@Autowired
+	private SocialMetaTagService service;
+	
+	@PostMapping("/info")
+	public ResponseEntity<SocialMetaTag> getDadosViaUrl(@RequestParam("url") String url) {
+		
+		SocialMetaTag socialMetaTag = service.getSocialMetaTagByUrl(url);
+		return socialMetaTag != null
+				? ResponseEntity.ok(socialMetaTag)
+				: ResponseEntity.notFound().build();
+	}
+}
