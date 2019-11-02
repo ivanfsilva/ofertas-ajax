00fad3567d58ee9fd3c034bd660150252a75f67a
ivanfsilva <ivanfs27@gmail.com>
2019-11-02 11:12:01 -0300

    validação do Back-End e Ajax

--- src/main/java/br/com/ivanfsilva/ofertasajax/domain/Promocao.java
+++ src/main/java/br/com/ivanfsilva/ofertasajax/domain/Promocao.java
@@ -5,6 +5,8 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.NumberFormat;
 import org.springframework.format.annotation.NumberFormat.Style;
@@ -17,9 +19,11 @@ public class Promocao implements Serializable {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank(message = "Um título é requerido")
 	@Column(name = "titulo", nullable = false)
 	private String titulo;
 	
+	@NotBlank(message = "O link da promoção é requerido")
 	@Column(name = "link_promocao", nullable = false)
 	private String linkPromocao;
 	
@@ -32,6 +36,7 @@ public class Promocao implements Serializable {
 	@Column(name = "link_imagem", nullable = false)
 	private String linkImagem;
 	
+	@NotNull(message = "O preço é requerido")
 	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
 	@Column(name = "preco_promocao", nullable = false)
 	private BigDecimal preco;
@@ -42,6 +47,7 @@ public class Promocao implements Serializable {
 	@Column(name = "data_cadastro", nullable = false)
 	private LocalDateTime dtCadastro;
 	
+	@NotNull(message = "Uma categoria é requerida")
 	@ManyToOne
 	@JoinColumn(name = "categoria_fk")
 	private Categoria categoria;
--- src/main/java/br/com/ivanfsilva/ofertasajax/web/controller/PromocaoController.java
+++ src/main/java/br/com/ivanfsilva/ofertasajax/web/controller/PromocaoController.java
@@ -1,13 +1,19 @@
 package br.com.ivanfsilva.ofertasajax.web.controller;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,7 +36,17 @@ public class PromocaoController {
 	private CategoriaRepository categoriaRepository;
 	
 	@PostMapping("/save")
-	public ResponseEntity<Promocao> salvarPromocao(Promocao promocao) {
+	public ResponseEntity<?> salvarPromocao(@Valid Promocao promocao, BindingResult result) {
+		
+		if (result.hasErrors()) {
+			
+			Map<String, String> errors = new HashMap<>();
+			for (FieldError error : result.getFieldErrors()) {
+				errors.put(error.getField(), error.getDefaultMessage());
+			}
+			
+			return ResponseEntity.unprocessableEntity().body(errors);
+		}
 		
 		log.info("Promocao {}", promocao.toString());
 		promocao.setDtCadastro(LocalDateTime.now());
@@ -50,4 +66,3 @@ public class PromocaoController {
 		return "promo-add";
 	}
 }
-
