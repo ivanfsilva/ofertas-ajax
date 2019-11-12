37e9a86495304db1a15f89b55aa21cbe241f44f8
ivanfsilva <ivanfs27@gmail.com>
2019-11-11 22:36:40 -0300

    Infinite Scroll em acao

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -1,6 +1,12 @@
 
 // efeito infinite scroll
 
+$(document).ready(function() {
+	$("#loader-img").hide();
+	$("#fim-btn").hide;
+});
+
+
 var pageNumber = 0;
 
 $(window).scroll(function(){
@@ -24,8 +30,25 @@ function loadByScrollBar(pageNumber) {
 		data: {
 			page: pageNumber
 		},
+		beforeSend: function(){
+			$("#loader-img").show();
+		},
 		success: function(response) {
 			console.log("resposta > ", response);
+			if(response.lengh > 150) {
+				$(".row").fadeIn(250, function(){
+					$(this).append(response);
+				});
+			} else {
+				$("#fim-btn").show;
+				$("#loader-img").remove("loader");
+			}
+		},
+		error: function(xhr){
+			alert("Ops, ocorreu um erro: " + xhr.status + " - " + xhr.statusText);
+		},
+		complete: function() {
+			$("#loader-img").hide();
 		}
 	})
 }
