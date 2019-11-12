1133f57205a80171f9328a12fa87149682b7782e
ivanfsilva <ivanfs27@gmail.com>
2019-11-11 22:16:02 -0300

    Funcao Ajax para paginacao via scroll

--- src/main/java/br/com/ivanfsilva/ofertasajax/web/controller/PromocaoController.java
+++ src/main/java/br/com/ivanfsilva/ofertasajax/web/controller/PromocaoController.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.com.ivanfsilva.ofertasajax.domain.Categoria;
 import br.com.ivanfsilva.ofertasajax.domain.Promocao;
@@ -43,6 +44,14 @@ public class PromocaoController {
 		PageRequest pageRequest = PageRequest.of(0, 8);
 		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
 		return "promo-list";
+	}
+	
+	@GetMapping("/list/ajax")
+	public String listarCards(@RequestParam(name = "page", defaultValue = "1") int page, ModelMap model) {
+//		Sort sort = new Sort(Sort.Direction.DESC, "dtCadastro");
+		PageRequest pageRequest = PageRequest.of(page, 8);
+		model.addAttribute("promocoes", promocaoRepository.findAll(pageRequest));
+		return "promo-card";
 	}
 	
 	@PostMapping("/save")
--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -1,13 +1,31 @@
 
 // efeito infinite scroll
 
+var pageNumber = 0;
+
 $(window).scroll(function(){
 	
 	var scrollTop = $(this).scrollTop();
 	var conteudo = $(document).height() - $(window).height();
 	
 	if(scrollTop >= conteudo) {
+		pageNumber++;
+		setTimeout(fuction(){
+			loadByScrollBar(pageNumber);
+		}, 200);
 		console.log("***");
-		
 	}
 });
+
+function loadByScrollBar(pageNumber) {
+	$.ajax({
+		method: "GET",
+		url: "/promocao/list/ajax",
+		data: {
+			page: pageNumber
+		},
+		success: function(response) {
+			console.log("resposta > ", response);
+		}
+	})
+}
