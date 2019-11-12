690d19d0b1d8eade3d165b0936cf569a5044c7b4
ivanfsilva <ivanfs27@gmail.com>
2019-11-11 22:53:41 -0300

    Operacao para likes

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -52,3 +52,9 @@ function loadByScrollBar(pageNumber) {
 		}
 	})
 }
+
+// adicionar likes
+$("button[id*='likes-btn-]").on("click", function(){
+	var id = $(this).attr("id").split("-")[2];
+	console.log("id: ", id);
+});
