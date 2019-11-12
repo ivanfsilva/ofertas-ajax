6b11d59f97b369fed5aea76a6107a1981efcd5bf
ivanfsilva <ivanfs27@gmail.com>
2019-11-11 23:17:29 -0300

    Metodos update e de consulta para likes

--- src/main/java/br/com/ivanfsilva/ofertasajax/repository/PromocaoRepository.java
+++ src/main/java/br/com/ivanfsilva/ofertasajax/repository/PromocaoRepository.java
@@ -1,9 +1,21 @@
 package br.com.ivanfsilva.ofertasajax.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.ivanfsilva.ofertasajax.domain.Promocao;
 
 public interface PromocaoRepository extends JpaRepository<Promocao, Long> {
+	
+	@Transactional(readOnly = false)
+	@Modifying
+	@Query("UPDATE Promocao p SET p.likes = p.likes + 1 WHERE p.id = :id")
+	void updateSomarLikes(@Param("id") Long id);
+	
+	@Query("SELECT p.likes FROM Promocao p WHERE p.id = :id")
+	int findLikesById(@Param("id") Long id);
 
 }
