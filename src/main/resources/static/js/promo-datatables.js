1ced373500dcd7ed91d90a3301e108c6b0dcd083
ivanfsilva <ivanfs27@gmail.com>
2019-12-04 20:34:42 -0300

    Integrando a DWR ao Spring

--- src/main/resources/static/js/promo-datatables.js
+++ src/main/resources/static/js/promo-datatables.js
@@ -5,6 +5,38 @@ $(document).ready(function(){
         serverSide: true,
         responsive: true,
         lengthMenu: [ 10, 15, 20, 25 ],
+        "oLanguage": {
+            "sProcessing":   "Processando...",
+            "sLengthMenu":   "Mostrar _MENU_ registros",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords":  "Não foram encontrados resultados",
+            "sInfo":         "Mostrando de _START_ até _END_ de _TOTAL_ registros",
+            "sInfoEmpty":    "Mostrando de 0 até 0 de 0 registros",
+            "sInfoFiltered": "",
+            "sInfoPostFix":  "",
+            "sInfoThousands": ".",
+            "sInfoComma": ".",
+            "sSearch":       "Buscar:",
+            "sUrl":          "",
+            "oPaginate": {
+                "sFirst":    "Primeiro",
+                "sPrevious": "Anterior",
+                "sNext":     "Seguinte",
+                "sLast":     "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            },
+            "select": {
+                "rows": {
+                    "_": "Selecionado %d linhas",
+                    "0": "Nenhuma linha selecionada",
+                    "1": "Selecionado 1 linha"
+                }
+            }
+        },
         ajax: {
             url: "/promocao/datatables/server",
             data: "data"
